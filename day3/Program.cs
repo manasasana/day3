ed87d10c476df8c9fd96b358e28a13c1a308e0d2
manasasana <manasaadupa123@gmail.com>
2022-08-10 15:22:36 +0530

    Add project files.

---
+++ day3/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Exceptionhand
+{
+    public void validateage(int age)
+    {
+        if (age < 18)
+        {
+            throw new underageException("You are underage");
+        }
+
+    }
+
+    public static void Main(String[] args)
+    {
+        try
+        {
+            validateage(17);
+        }
+        catch (underageException e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("code is resumed");
+        }
+
+    }
+
+}
+      
+    public class underageException : Exception
+    {
+        public underageException(string msg);base(msg):
+        {
+
+        }
+    }
+
+class test
+{
+    public static void Main(String[] args)
+    {
+        show();
+    }
+    public static void show()
+    {
+        int[] arr = { 1, 2, 5, 8, 9, 6, 4, 10 };
+        //Array.Sort(arr);
+        Array.Reverse(arr);
+        for(int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
